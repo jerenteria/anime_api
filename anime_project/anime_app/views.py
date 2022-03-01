4899d9793808f19b36e4ee8fdf303d645b04a7ff
jerenteria <renteria.juan96@gmail.com>
2022-02-28 20:23:45 -0800

    learning as I go

--- anime_project/anime_app/views.py
+++ anime_project/anime_app/views.py
@@ -3,22 +3,10 @@ from django.shortcuts import render, redirect
 import requests
 import json
 
+def getRepsonse(requests)
+    response = requests.get("http://api.open-notify.org/astros.json")
+    query = {'lat':'45', 'lon':'180'}
+    response = requests.get('http://api.open-notify.org/iss-pass.json', params=query)
+    print(response.json())
 
-# URL = "https://api.waifu.pics/type/category"
 
-# def index(request):
-#     url_json = requests.get(URL)
-#     data = json.loads(url_json.text)
-#     print(data)
-#     return render(request, "index.html")
-
-URL = "https://api.waifu.pics/type/category"
-
-def get_joke():
-    api_end_point = "https://official-joke-api.appspot.com/jokes/random"
-    joke = requests.get(api_end_point)
-    json_data = json.loads(joke.text)
-    print(json_data)
-
-if __name__ == "__main__":
-    get_joke()
